67f8203feccccef89b79435affdc34fe6dbdd938
Vedanta505 <fluenoriph@gmail.com>
2024-10-28 01:27:27 +1000

    Add files via upload

---
+++ create_database.ps1
@@ -0,0 +1,4 @@
+param ($db_name)
+chcp 65001
+Set-Location C:\
+.\sqlite3 $db_name
---
+++ open_database.ps1
@@ -0,0 +1,7 @@
+chcp 65001
+$database_name = 'physical_data.db'
+$database_path = 'C:\Users\Mahabhara\PycharmProjects\Lab_Calcs\' + $database_name
+$message = 'Current database - ' + $database_path
+$out_message_info = Write-Verbose -Message $message -Verbose
+$out_message
+C:\sqlite3
