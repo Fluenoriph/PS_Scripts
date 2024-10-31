6618651159a9afc13108477167d4bcb3959f5cd3
Vedanta505 <fluenoriph@gmail.com>
2024-10-31 21:00:25 +1000

    New mask list

--- backup_protocols_scans.ps1
+++ backup_protocols_scans.ps1
@@ -4,33 +4,38 @@ $source_path = $home + '\Desktop\сканы'
 
 $destination_path = $home + '\Desktop\result_test\'                              
 
-$monthes_folders = ('Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь') 
+$monthes_folders_names = ('Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь') 
 
 $month_mask = @{
     January = "^\d+-\w+-\d{2}\.01\.\d{4}\.pdf$"
-    February = "\d+-\w+-\d{2}\.02\.\d{4}\.jpg"
-    March = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf" 
-    April = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    May = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    June = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    July = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    August = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    September = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    October = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    November = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
-    December = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
+    February = "^\d+-\w+-\d{2}\.02\.\d{4}\.pdf$"
+    March = "^\d+-\w+-\d{2}\.03\.\d{4}\.pdf$" 
+    April = "^\d+-\w+-\d{2}\.04\.\d{4}\.pdf$"
+    May = "^\d+-\w+-\d{2}\.05\.\d{4}\.pdf$"
+    June = "^\d+-\w+-\d{2}\.06\.\d{4}\.pdf$"
+    July = "^\d+-\w+-\d{2}\.07\.\d{4}\.pdf$"
+    August = "^\d+-\w+-\d{2}\.08\.\d{4}\.pdf$"
+    September = "^\d+-\w+-\d{2}\.09\.\d{4}\.pdf$"
+    October = "^\d+-\w+-\d{2}\.10\.\d{4}\.pdf$"
+    November = "^\d+-\w+-\d{2}\.11\.\d{4}\.pdf$"
+    December = "^\d+-\w+-\d{2}\.12\.\d{4}\.pdf$"
 }
+
+$eias_month_mask = ("^\d{5}-\d{2}-\d{2}-\d{2}\.01\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.02\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.03\.\d{4}\.pdf$", 
+                    "^\d{5}-\d{2}-\d{2}-\d{2}\.04\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.05\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.06\.\d{4}\.pdf$", 
+                    "^\d{5}-\d{2}-\d{2}-\d{2}\.07\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.08\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.09\.\d{4}\.pdf$", 
+                    "^\d{5}-\d{2}-\d{2}-\d{2}\.10\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.11\.\d{4}\.pdf$", "^\d{5}-\d{2}-\d{2}-\d{2}\.12\.\d{4}\.pdf$")
 
 $simple_files_count = 0; $eias_files_count = 0
 
 if ($month_value -eq 01) {
-    $result_folder = $destination_path + $monthes_folders[0]
+    $result_folder = $destination_path + $monthes_folders_names[0]
 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | Copy-Item -Destination $result_folder 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object Name | Out-File -FilePath .\logging.txt
     Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object {$simple_files_count += 1}
     
-    $month_mask.January = "^\d{5}-\d{2}-\d{2}-\d{2}\.01\.\d{4}\.pdf$"
+    $month_mask.January = $eias_month_mask[0]
 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | Copy-Item -Destination $result_folder 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object Name | Out-File -FilePath .\logging.txt -Append
