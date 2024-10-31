8f3d77c988324d5c5ff10355b3fb9cd8f53e62fe
Vedanta505 <fluenoriph@gmail.com>
2024-10-31 20:37:50 +1000

    Fixed copy all protocols and loging

--- backup_protocols_scans.ps1
+++ backup_protocols_scans.ps1
@@ -1,14 +1,13 @@
-#chcp 65001
-#param ($source_type)
-$source_type = 'C:\Users\Mahabhara\Desktop\сканы' 
+param($month_value)
 
-#param ($destination_path)  
-$destination_path = $home + "\Desktop\result_test\Январь"                              
+$source_path = $home + '\Desktop\сканы' 
 
-# $monthes_paths = ('\январь', '\февраль', '\март', '\апрель', '\май', '\июнь', '\июль', '\август', '\сентябрь', '\октябрь', '\ноябрь', '\декабрь')    ???????
+$destination_path = $home + '\Desktop\result_test\'                              
 
-$monthes_dict = @{
-    January = "\d+-\w+-\d{2}\.01\.\d{4}\.jpg"
+$monthes_folders = ('Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь') 
+
+$month_mask = @{
+    January = "^\d+-\w+-\d{2}\.01\.\d{4}\.pdf$"
     February = "\d+-\w+-\d{2}\.02\.\d{4}\.jpg"
     March = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf" 
     April = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
@@ -22,11 +21,24 @@ $monthes_dict = @{
     December = "\d+-\w+-\d{2}\.01\.\d{4}\.pdf"
 }
 
-Get-ChildItem $source_type | Where-Object Name -Match $monthes_dict.January | Copy-Item -Destination $destination_path
+$simple_files_count = 0; $eias_files_count = 0
 
+if ($month_value -eq 01) {
+    $result_folder = $destination_path + $monthes_folders[0]
 
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | Copy-Item -Destination $result_folder 
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object Name | Out-File -FilePath .\logging.txt
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object {$simple_files_count += 1}
+    
+    $month_mask.January = "^\d{5}-\d{2}-\d{2}-\d{2}\.01\.\d{4}\.pdf$"
 
-
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | Copy-Item -Destination $result_folder 
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object Name | Out-File -FilePath .\logging.txt -Append
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $month_mask.January | ForEach-Object {$eias_files_count += 1}
+        
+    $result_count = "`nКоличество скопированных файлов: " + ($simple_files_count + $eias_files_count)
+    $result_count | Out-File -FilePath .\logging.txt -Append
+}
 
 
 
