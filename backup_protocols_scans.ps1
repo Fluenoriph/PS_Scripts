928d4dba735c54d14d743857642a38a5341404a3
Vedanta505 <fluenoriph@gmail.com>
2024-11-01 23:41:58 +1000

    Logging backup func

--- backup_protocols_scans.ps1
+++ backup_protocols_scans.ps1
@@ -1,8 +1,8 @@
 param($month_value)
 
-$global:source_path = $home + '\Desktop\сканы' 
+$source_path = $home + '\Desktop\сканы' 
 
-$global:destination_path = $home + '\Desktop\result_test\'                              
+$destination_path = $home + '\Desktop\result_test\'                              
 
 $month_mask = ("^\d+-\w+-\d{2}\.01\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.02\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.03\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.04\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.05\.\d{4}\.pdf$",
                "^\d+-\w+-\d{2}\.06\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.07\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.08\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.09\.\d{4}\.pdf$", "^\d+-\w+-\d{2}\.10\.\d{4}\.pdf$", 
@@ -18,19 +18,17 @@ $monthes_folders_names = ('Январь', 'Февраль', 'Март', 'Апр�
 $log_files_names = ('.\january_backup_log.txt', '.\february_backup_log.txt', '.\march_backup_log.txt', '.\april_backup_log.txt', '.\may_backup_log.txt', '.\june_backup_log.txt', 
                     '.\july_backup_log.txt', '.\august_backup_log.txt', '.\september_backup_log.txt', '.\october_backup_log.txt', '.\november_backup_log.txt', '.\december_backup_log.txt')
 
-$global:log_file
 
 function backuping_files ($file_mask, $eias_file_mask, $folder_name) {
-    $result_folder = $destination_path + $folder_name
     $simple_files_count = 0; $eias_files_count = 0
                         
-    Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | Copy-Item -Destination $result_folder 
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | Copy-Item -Destination $destination_path$folder_name 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | ForEach-Object Name | Out-File -FilePath $log_file
     Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | ForEach-Object {$simple_files_count += 1}
                         
     $file_mask = $eias_file_mask
                     
-    Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | Copy-Item -Destination $result_folder 
+    Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | Copy-Item -Destination $destination_path$folder_name 
     Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | ForEach-Object Name | Out-File -FilePath $log_file -Append
     Get-ChildItem -Path $source_path | Where-Object Name -Match $file_mask | ForEach-Object {$eias_files_count += 1}
                             
@@ -38,76 +36,80 @@ function backuping_files ($file_mask, $eias_file_mask, $folder_name) {
     $result_count | Out-File -FilePath $log_file -Append
 }
 
-if ($month_value -eq 01) {
-    $log_file = $log_files_names[0]
 
-    backuping_files $month_mask.January $eias_month_mask[0] $monthes_folders_names[0]
-}
-elseif ($month_value -eq 02) {
-    $log_file = $log_files_names[1]
-
-    backuping_files $month_mask.February $eias_month_mask[1] $monthes_folders_names[1]
-}
-elseif ($month_value -eq 03) {
-    $log_file = $log_files_names[2]
-
-    backuping_files $month_mask.March $eias_month_mask[2] $monthes_folders_names[2]
-}
-elseif ($month_value -eq 04) {
-    $log_file = $log_files_names[3]
-
-    backuping_files $month_mask.April $eias_month_mask[3] $monthes_folders_names[3]
-}
-elseif ($month_value -eq 05) {
-    $log_file = $log_files_names[4]
-
-    backuping_files $month_mask.May $eias_month_mask[4] $monthes_folders_names[4]
-}
-elseif ($month_value -eq 06) {
-    $log_file = $log_files_names[5]
-
-    backuping_files $month_mask.June $eias_month_mask[5] $monthes_folders_names[5]
-}
-elseif ($month_value -eq 07) {
-    $log_file = $log_files_names[6]
-
-    backuping_files $month_mask.July $eias_month_mask[6] $monthes_folders_names[6]
-}
-elseif ($month_value -eq 08) {
-    $log_file = $log_files_names[7]
-
-    backuping_files $month_mask.August $eias_month_mask[7] $monthes_folders_names[7]
-}
-elseif ($month_value -eq 09) {
-    $log_file = $log_files_names[8]
-
-    backuping_files $month_mask.September $eias_month_mask[8] $monthes_folders_names[8]
-}
-elseif ($month_value -eq 10) {
-    $log_file = $log_files_names[9]
-
-    backuping_files $month_mask.October $eias_month_mask[9] $monthes_folders_names[9]
-}
-elseif ($month_value -eq 11) {
-    $log_file = $log_files_names[10]
-
-    backuping_files $month_mask.November $eias_month_mask[10] $monthes_folders_names[10]
-}
-elseif ($month_value -eq 12) {
-    $log_file = $log_files_names[11]
-
-    backuping_files $month_mask.December $eias_month_mask[11] $monthes_folders_names[11]
-}
+function logging_backup ()
 
 
-function backuping_per_year ($file_mask, $eias_file_mask, $folder_name) {
+function backuping_per_year {
     $i = 0
     
     while ($i -le 11) {
         $log_file = $log_files_names[$i]
 
-        backuping_files $file_mask[$i] $eias_file_mask[$i] $folder_name[$i]
+        backuping_files $month_mask[$i] $eias_month_mask[$i] $monthes_folders_names[$i]
 
         $i++
     }
+}
+
+
+if ($month_value -eq 01) {
+    $log_file = $log_files_names[0]
+
+    backuping_files $month_mask[0] $eias_month_mask[0] $monthes_folders_names[0]
+}
+elseif ($month_value -eq 02) {
+    $log_file = $log_files_names[1]
+
+    backuping_files $month_mask[1] $eias_month_mask[1] $monthes_folders_names[1]
+}
+elseif ($month_value -eq 03) {
+    $log_file = $log_files_names[2]
+
+    backuping_files $month_mask[2] $eias_month_mask[2] $monthes_folders_names[2]
+}
+elseif ($month_value -eq 04) {
+    $log_file = $log_files_names[3]
+
+    backuping_files $month_mask[3] $eias_month_mask[3] $monthes_folders_names[3]
+}
+elseif ($month_value -eq 05) {
+    $log_file = $log_files_names[4]
+
+    backuping_files $month_mask[4] $eias_month_mask[4] $monthes_folders_names[4]
+}
+elseif ($month_value -eq 06) {
+    $log_file = $log_files_names[5]
+
+    backuping_files $month_mask[5] $eias_month_mask[5] $monthes_folders_names[5]
+}
+elseif ($month_value -eq 07) {
+    $log_file = $log_files_names[6]
+
+    backuping_files $month_mask[6] $eias_month_mask[6] $monthes_folders_names[6]
+}
+elseif ($month_value -eq 08) {
+    $log_file = $log_files_names[7]
+
+    backuping_files $month_mask[7] $eias_month_mask[7] $monthes_folders_names[7]
+}
+elseif ($month_value -eq 09) {
+    $log_file = $log_files_names[8]
+
+    backuping_files $month_mask[8] $eias_month_mask[8] $monthes_folders_names[8]
+}
+elseif ($month_value -eq 10) {
+    $log_file = $log_files_names[9]
+
+    backuping_files $month_mask[9] $eias_month_mask[9] $monthes_folders_names[9]
+}
+elseif ($month_value -eq 11) {
+    $log_file = $log_files_names[10]
+
+    backuping_files $month_mask[10] $eias_month_mask[10] $monthes_folders_names[10]
+}
+elseif ($month_value -eq 12) {
+    $log_file = $log_files_names[11]
+
+    backuping_files $month_mask[11] $eias_month_mask[11] $monthes_folders_names[11]
 }
--- sqlite_script.ps1
+++ sqlite_script.ps1
@@ -1,12 +1,13 @@
-function create_the_database ($name) {
-    $global:database_name = $name
+chcp 65001
 
+
+function create_the_database ($name) {
     Set-Location C:\
-    .\sqlite3 $database_name
+    .\sqlite3 $name
 } 
 
 
-function open_the_database ($current_name = $database_name) {
+function open_the_database ($current_name = 'register_data.db') {
     $database_path = 'C:\Users\Mahabhara\PycharmProjects\Lab_Calcs\' + $current_name
     $message = 'Current database - ' + $database_path
     $out_message = Write-Verbose -Message $message -Verbose
